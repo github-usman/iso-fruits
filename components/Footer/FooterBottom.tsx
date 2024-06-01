a9fe32cf1e5cca45df5f40726263149c0a12233f
Aman Gupta <khwaabknight@gmail.com>
2024-06-01 13:07:45 +0530

    Added Home page isolated banana , footer responsive

---
+++ components/Footer/FooterBottom.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const FooterBottom = () => {
+  return (
+    <>
+      <div className='copyright'>
+        <p>Copyright © 2024 JustStart All Rights Reserved.</p>
+      </div>
+      <div className="more-details">
+        <div className="container">
+          <button type='button'>
+            <p>
+              get more details
+            </p>
+          </button>
+          <button type='button'>
+            live chat
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FooterBottom
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import NextNProgress from "nextjs-progressbar";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Footer from "../components/Footer";
+import NewsLetterCTA from "../components/Home/NewsLetterCTA";
+import IsolatedBanana from "../components/Home/IsolatedBanana";
 
 export default function Home() {
   return (
@@ -9,6 +12,9 @@ export default function Home() {
         <NextNProgress color="#ffa229" height={5} />
         <Header/>
         <Hero />
+        <IsolatedBanana/>
+        <NewsLetterCTA/>
+        <Footer/>
       </>
     </div>
   );
