d57842dfee773f0f317169bbe3c5aef64e4f9ddc
github-usman <usmantoyou@gmail.com>
2024-06-02 12:27:23 +0530

    product page add

--- components/static/iso-fruits/product/Products.tsx
+++ components/static/iso-fruits/product/Products.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
 import React from "react";
+import yellow_banana_powder from "../../../../assets/images/static/iso-fruits/product/yellow_banana_powder.svg";
+import green_banana_powder from "../../../../assets/images/static/iso-fruits/product/green_banana_powder.png";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="iso-fruits-product">
+      <div className="top-card">
+        <Image src={yellow_banana_powder} alt="yellow banana powder" />
+        <div>
+            <h2>Yellow Banana Powder</h2>
+            <p><span>Color </span>: Yellow</p>
+            <p><span>Packaging Size </span>: 1kg, 50 Kg</p>
+            <p><span>Cultivation Type </span>: Natural</p>
+            <p><span>Shelf Life </span>: 1 year</p>
+            <p><span>Feature </span>: Pure, Healthy, Good Taste</p>
+        </div>
+      </div>
+      <div className="bottom-card">
+        <Image src={green_banana_powder} alt="green banana powder" />
+        <div>
+            <h2>Green Banana Powder</h2>
+            <p><span>Color</span> : Green</p>
+            <p><span>Packaging Size</span> : 1kg, 50 Kg</p>
+            <p><span>Cultivation Type</span> : Natural</p>
+            <p><span>Shelf Life</span> : 1 year</p>
+            <p><span>Feature</span> : Pure, Healthy, Good Taste</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
