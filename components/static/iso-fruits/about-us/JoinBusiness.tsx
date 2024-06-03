c1bdf09797693cea2a6f181ab7b919c9903a97a4
itzsourab <sv059813@gmail.com>
2024-06-03 17:38:11 +0530

    joinbusiness:content

---
+++ components/static/iso-fruits/about-us/JoinBusiness.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const JoinBusiness = () => {
+  return (
+    
+    <div className='join-bigcontainer'>
+      <div className='join-container'>
+        <p className='join-text-container'>
+        Join the growing number of businesses that trust ISO Fruites for their nutritional product needs. Contact our B2B team to learn more about our partnership opportunities and how we can help you achieve your business goals.
+        </p>
+    </div>
+    </div>
+  )
+}
+
+export default JoinBusiness
--- pages/iso-fruits/about-us.tsx
+++ pages/iso-fruits/about-us.tsx
@@ -1,4 +1,5 @@
 import AboutUs from "../../components/static/iso-fruits/about-us/AboutUs";
+import JoinBusiness from "../../components/static/iso-fruits/about-us/JoinBusiness";
 import WhyChooseUs from "../../components/static/iso-fruits/about-us/WhyChooseUs";
 
 
@@ -7,6 +8,7 @@ const Home: React.FC = () => {
     return (
       <>
         <AboutUs/>
+        <JoinBusiness/>
         <WhyChooseUs/>
       </>
     );
