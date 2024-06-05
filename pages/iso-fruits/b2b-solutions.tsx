fdebc5ab91c0a51155a39cf0ca9fb4bdcad032e1
Aman Gupta <khwaabknight@gmail.com>
2024-06-05 17:23:08 +0530

    b2b-solutions and contact-us page mobile and tablet design

--- pages/iso-fruits/b2b-solutions.tsx
+++ pages/iso-fruits/b2b-solutions.tsx
@@ -9,13 +9,13 @@ const Home: React.FC = () => {
           <p>At ISO Fruits, we understand the unique needs of businesses in the health and wellness industry. Our premium banana fitness powder is not only a favorite among individual consumers but also a valuable addition to a wide range of business offerings. Whether you're in retail, hospitality, or health and fitness, partnering with us can enhance your product portfolio and drive customer satisfaction.</p>
           <p><span>Industries We Serve</span></p>
 
-          <p><span>Retailers and Distributors</span>: Expand your product lineup with our high-demand banana fitness powder. We offer attractive wholesale pricing and promotional support to help you succeed.</p>
+          <p><span className="subheading-span">Retailers and Distributors</span>: Expand your product lineup with our high-demand banana fitness powder. We offer attractive wholesale pricing and promotional support to help you succeed.</p>
 
-          <p><span>Gyms and Fitness Centers</span>: Enhance your nutritional offerings with our fitness powder, perfect for post-workout recovery and meal replacements. Provide your clients with a product they can trust.</p>
+          <p><span className="subheading-span">Gyms and Fitness Centers</span>: Enhance your nutritional offerings with our fitness powder, perfect for post-workout recovery and meal replacements. Provide your clients with a product they can trust.</p>
 
-          <p><span>Health and Wellness Stores</span>: Stock your shelves with our premium products that align with the health-conscious preferences of your customers.</p>
+          <p><span className="subheading-span">Health and Wellness Stores</span>: Stock your shelves with our premium products that align with the health-conscious preferences of your customers.</p>
 
-          <p><span>Cafes and Smoothie Bars</span>: Integrate our banana fitness powder into your menu for delicious and nutritious smoothies, shakes, and health drinks that your patrons will love.</p>
+          <p><span className="subheading-span">Cafes and Smoothie Bars</span>: Integrate our banana fitness powder into your menu for delicious and nutritious smoothies, shakes, and health drinks that your patrons will love.</p>
 
           <p><span>Partner with Us Today</span></p>
 
@@ -23,9 +23,9 @@ const Home: React.FC = () => {
 
           <p><span>Contact Our B2B Team</span></p>
 
-          <p>Email: email@gmail.com </p>
+          <p className="email"><span className="subheading-span">Email:</span> email@gmail.com </p>
 
-         <p> Phone: +919872461839</p>
+         <p><span className="subheading-span">Phone:</span> +919872461839</p>
       </div>
     );
   };
--- pages/iso-fruits/contact-us.tsx
+++ pages/iso-fruits/contact-us.tsx
@@ -6,8 +6,14 @@ const Home: React.FC = () => {
     return (
       <div className="iso-fruites-contact-us">
         <h5>We’d love to hear from you! Whether you have a question, need assistance, or want to share feedback, our team is here to help</h5>
-        <div className="email-container"><Image src={email} alt={'email icons'} ></Image> <span>email - </span><p>&nbsp;  isofruits.powder@gmail.com</p></div>
-        <div className="call-container"><Image src={call} alt={'call icons'} ></Image> <span>mobile No.</span> <p>&nbsp; +917011575046</p> </div>
+        <div className="email-container">
+          <Image src={email} alt={'email icons'} />
+          
+          <p><span>email - </span>isofruits.powder@gmail.com</p>
+        </div>
+        <div className="call-container">
+          <Image src={call} alt={'call icons'} />
+          <p><span>mobile No.</span>  +917011575046</p> </div>
       </div>
     );
   };
