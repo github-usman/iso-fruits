d41b5ab7dfde84e01829dcbad26f482992f85b5f
codingpartnerNB <nehab5314@gmail.com>
2024-06-06 13:02:37 +0530

    Neha : Adding Partner with us today page

---
+++ pages/iso-fruits/partner-with-us-today.tsx
@@ -0,0 +1,12 @@
+
+const Home: React.FC = () => {
+    return (
+      <div className="iso-fruites-partner-with-us-today">
+        <p>
+            Join the growing number of businesses that trust ISO Fruites for their nutritional product needs. Contact our B2B team to learn more about our partnership opportunities and how we can help you achieve your business goals.
+        </p>
+      </div>
+    );
+  };
+  
+  export default Home;
