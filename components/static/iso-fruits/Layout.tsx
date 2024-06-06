5e2696f6a3f1662313cc62b7b5d2f469a6cc7efe
Aman Gupta <khwaabknight@gmail.com>
2024-06-06 18:28:42 +0530

    changes asked by vipin

--- components/static/iso-fruits/Layout.tsx
+++ components/static/iso-fruits/Layout.tsx
@@ -1,18 +1,50 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import NextNProgress from 'nextjs-progressbar';
 import Header from './Header/Header';
 import Footer from './Footer';
+import Image from 'next/image'
+import whatsapp from '../../../assets/images/static/iso-fruits/Footer/whatsapp.svg'
 
 interface LayoutProps {
     children: ReactNode;
   }
   
   const Layout: React.FC<LayoutProps> = ({ children }) => {
+
+    const [showText, setShowText] = useState(false);
+
+    const handleMouseEnter = () => {
+      setShowText(true);
+    };
+  
+    const handleScroll = () => {
+      setShowText(false);
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+    
     return (
       <div>
         <NextNProgress color="#ffa229" height={5} />
         <Header />
-        <main>{children}</main>
+        <main className='main-conatiner-iso-fruits'>
+          {children}
+          <button className='whatsapp-btn'>
+            <a href='https://wa.me/9873461839' target='_blank' rel='noreferrer noopener'
+              onMouseEnter={handleMouseEnter}
+            >
+              <Image src={whatsapp} alt='whatsapp-logo' />
+              {
+                showText && <p className={showText ? 'visible-text' : 'hidden-text'}>WhatsApp us</p>
+              }
+            </a>
+          </button>
+        </main>
         <Footer />
       </div>
     );
